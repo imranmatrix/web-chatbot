5e1883f97a2537c00f0c5dddc9d43f87d1a94a99
Imran Shaikh <imran.s@graymatrix.com>
2024-07-12 18:09:39 +0530

    file updates

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -8,5 +8,36 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+
+  ngOnInit() {
+    this.loadChatWidget();
+  }
+
+  loadChatWidget(token=false) {
+    // Set global variables
+    (window as any).chat_widget_reference_no = '6rtket9h6nh66thr61jp8db16mtk2e9tc8wp8t8';
+    (window as any).bot_type = 'Full';
+    (window as any).token = token;
+
+
+    // Load the chat widget script
+    const scriptPing = document.createElement('script');
+    scriptPing.type = 'text/javascript';
+    scriptPing.src = 'https://talkk7uatchat.talkk.in/ping';
+    document.body.appendChild(scriptPing);
+
+    // Load the chat widget CSS
+    const linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.href = 'https://talkk7uatwebchat.talkk.in/index.css';
+    document.head.appendChild(linkElement);
+
+    // Load the chat widget JS
+    const scriptWidget = document.createElement('script');
+    scriptWidget.src = 'https://talkk7uatwebchat.talkk.in/index.js';
+    scriptWidget.defer = true;
+    document.body.appendChild(scriptWidget);
+  }
+
 
 }
